4d25fafd691cabfe1aef36ba27870569566daf81
vlatko <vlatko@mono.hr>
2017-03-02 16:52:25 +0100

    added angular http service added baasic module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import * as angular from "angular";
-import * as services from "./services";
+import { getHttpService } from "./services/httpService";
+import { BaasicApp, IBaasicAppOptions } from "baasic-sdk-core";
 
-var apiModule = angular.module("baasic.api", []);
-services.register(apiModule);
+angular.module("baasic.api", [])
+    .provider("baasicApp", (() => {
+        let configuration;
+        this.configure = (apiKey:string, options?: IBaasicAppOptions) => {
+            configuration = {
+                apiKey: apiKey,
+                options: options
+            };
+        };
 
-
-
-
+        this.$get = ["$http", ($http) => {
+            if (configuration) {
+                var options : Partial<IBaasicAppOptions> = angular.extend({}, configuration.options, {
+                    httpClient: getHttpService($http)
+                });
+                
+                return new BaasicApp(configuration.apiKey, options);
+            } else {
+                throw "You must configure baasic application";
+            }
+        }];
+    }) as angular.IServiceProviderFactory);
