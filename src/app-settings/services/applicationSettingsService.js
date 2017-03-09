02a2e69249a941e6e9e257ddb726e24c730bed8c
Kristijan Horvat <kristijan@mono.hr>
2017-03-09 15:01:35 +0100

    Working on module integrations

--- src/app-settings/services/applicationSettingsService.js
+++ src/app-settings/services/applicationSettingsService.js
@@ -2,12 +2,13 @@
 /**
  * @module baasicApplicationSettingsService
  * @description Baasic Application Settings Service provides an easy way to consume Baasic Application Settings REST API end-points. In order to obtain needed routes `baasicApplicationSettingsService` uses `baasicApplicationSettingsRouteService`.
-*/
+ */
 (function (angular, module, undefined) {
     'use strict';
-    module.service('baasicApplicationSettingsService', ['baasicApiHttp', 'baasicApiService', 'baasicConstants', 'baasicApplicationSettingsRouteService',
-        function (baasicApiHttp, baasicApiService, baasicConstants, applicationSettingsRouteService) {
-            return {    
+    module.service('baasicApplicationSettingsService', ['baasicApp',
+        function (baasicApps) {
+            var baasicApp = baasicApps.get();
+            return {
                 /**
                 * Returns a promise that is resolved once the get action has been performed. Success response returns the application settings resource.
                 * @method        
@@ -19,14 +20,14 @@ baasicApplicationSettingsService.get()
 .error(function (response, status, headers, config) {
   // perform error handling here
 });
-                **/  				
+                **/
                 get: function (options) {
-                    return baasicApiHttp.get(applicationSettingsRouteService.get.expand(baasicApiService.getParams(options)))
+                    return baasicApp.applicationSettings.get(options)
                         .success(function (appSettings) {
                             appSettings.origins = appSettings.origins || [];
                         });
                 },
-                 /**
+                /**
                  * Returns a promise that is resolved once the update application settings action has been performed. This action updates the application setting resource. This route uses HAL enabled objects to obtain routes and therefore it doesn't use `baasicApplicationSettingsRouteService` route template. Here is an example of how a route can be obtained from HAL enabled objects:
 ```
 var params = baasicApiService.removeParams(appSettings);
@@ -43,16 +44,14 @@ baasicApplicationSettingsService.update(appSettings)
 .error(function (response, status, headers, config) {
   // perform error handling here
 });
-				**/					
+				**/
                 update: function (data) {
-                    var params = baasicApiService.updateParams(data);
-					var model = params[baasicConstants.modelPropertyName];
-                    return baasicApiHttp.put(model.links('put').href, model);
+                    return baasicApp.applicationSettings.update(data);
                 },
-                
-				routeService: applicationSettingsRouteService
+                routeService: baasicApp.applicationSettings.routeDefinition
             };
-        }]);
+        }
+    ]);
 }(angular, module));
 
 /**
