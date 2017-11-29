8df691734c9b32a0e35a0376a9daa29121ea422b
Vlatka Fundak <vfundak@mono-software.com>
2017-11-29 15:51:18 +0100

    media gallery in separate module, added commertceProductSettingsService

---
+++ src/commerce/services/commerceProductSettingsService.js
@@ -0,0 +1,66 @@
+﻿/* globals module */
+/**
+ * @module baasicCommerceProductSettingsService
+ * @description Baasic Commerce Product Settings Service provides an easy way to consume Baasic Commerce Product Settings REST API end-points. In order to obtain needed routes `baasicCommerceProductSettingsService` uses `baasicCommerceProductSettingsRouteService`.
+ */
+(function (angular, module, undefined) {
+    'use strict';
+    module.service('baasicCommerceProductSettingsService', ['baasicApp',
+        function (baasicApps) {
+            var baasicApp = baasicApps.get();
+            return {
+                /**
+                 * Returns a promise that is resolved once the get action has been performed. Success response returns the product settings.
+                 * @method        
+                 * @example 
+baasicCommerceProductSettingsService.get()
+.success(function (data) {
+  // perform success action here
+})
+.error(function (response, status, headers, config) {
+  // perform error handling here
+});
+                **/
+                get: function (options) {
+                    return baasicApp.commerceModule.settings.get(options);
+                },
+                /**
+                 * Returns a promise that is resolved once the update product settings action has been performed; this action updates product settings. This route uses HAL enabled objects to obtain routes and therefore it doesn't apply `baasicCommerceProductSettingsRouteService` route template. Here is an example of how a route can be obtained from HAL enabled objects:
+```
+var params = baasicApiService.removeParams(productSettings);
+var uri = params['model'].links('put').href;
+```
+                 * @method        
+                 * @example 
+// productSettings is a resource previously fetched using get action.
+productSettings.allowArchive = true;
+baasicCommerceProductSettingsService.update(productSettings)
+.success(function (data) {
+  // perform success action here
+})
+.error(function (response, status, headers, config) {
+  // perform error handling here
+});
+				**/
+                update: function (data) {
+                    return baasicApp.commerceModule.settings.update(data);
+                },
+                /**
+                 * Provides direct access to `baasicCommerceProductSettingsRouteService`.
+                 * @method        
+                 * @example baasicCommerceProductSettingsService.routeService.get(expandObject);
+                 **/
+                routeService: baasicApp.commerceModule.settings.routeDefinition
+            };
+        }
+    ]);
+}(angular, module));
+/**
+ * @copyright (c) 2017 Mono Ltd
+ * @license MIT
+ * @author Mono Ltd
+ * @overview 
+ ***Notes:**
+ - Refer to the [Baasic REST API](http://dev.baasic.com/api/reference/home) for detailed information about available Baasic REST API end-points.
+ - All end-point objects are transformed by the associated route service.
+*/
---
+++ src/media-gallery/README.md
@@ -0,0 +1,26 @@
+# Baasic Media Gallery AngularJS SDK
+
+Baasic AngularJS Media Gallery library provides access to [Baasic REST API](http://dev.baasic.com/api/reference/home) media gallery end-points.
+
+## Initialization
+
+To be able to use the library you will need to add the Baasic (_baasic.mediaGallery_) dependency to your AngularJS module.
+
+```javascript
+angular.module('my-module', ["baasic.api", "baasic.mediaGallery"])
+```
+## Build Process
+
+1. Install [NodeJs](http://nodejs.org/download/)
+2. Open Shell/Command Prompt in the Baasic AngularJS folder
+3. Run `npm install`
+4. Install gulp globally: `npm install -g gulp`
+5. Run `gulp`
+
+## Contributing
+
+* [Pull requests are always welcome](../../../baasic-sdk-angularjs/pulls)
+* Please [report](../../../baasic-sdk-angularjs/issues) any issues you might have found
+* Help us write the documentation
+* Create interesting apps using SDK
+* Looking for something else to do? Get in <u>touch</u> ...
