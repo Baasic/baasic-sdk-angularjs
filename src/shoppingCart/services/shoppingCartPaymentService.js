652bf41ab3d613d601f6a2b664f3dd869a9c67df
Benjamin Suk <bsuk@mono-software.com>
2017-11-30 10:25:28 +0100

    Added payment service.

--- src/shoppingCart/services/shoppingCartPaymentService.js
+++ src/shoppingCart/services/shoppingCartPaymentService.js
@@ -10,14 +10,13 @@
             var baasicApp = baasicApps.get();
             return {
 /**
-                * Returns a promise that is resolved once the create shopping cart item action has been performed, this action creates a new shopping cart itemresource.
+                * Returns a promise that is resolved once the shopping cart item summary calculation action has been performed, this action calculates the sum for all items in the shopping cart.
                 * @method        
                 * @example 
-baasicShoppingCartPaymentService.create({
- productId : '<product-id>',
- quantity : '<quantity>',
- userId : '<user-id>',
- timeStamp : '<time-stamp>'
+baasicShoppingCartPaymentService.calculateSummary({
+ customerId : '<customer-id>',
+ systemName : '<system-name>',
+ coupon : '<coupon>'
 })
 .success(function (data) {
  // perform success action here
@@ -26,16 +25,17 @@ baasicShoppingCartPaymentService.create({
  // perform error handling here
 });
                 **/
-                create: function (data) {
-                    return baasicApp.shoppingCartModule.items.create(data);
+                calculateSummary: function (data) {
+                    return baasicApp.shoppingCartModule.payment.calculateSummary(data);
                 },
 /**
-                * Returns a promise that is resolved once the create shopping cart item action has been performed, this action creates a new shopping cart itemresource.
+                * Returns a promise that is resolved once the shopping cart item summary processing action has been performed, this action processes the items in the shopping cart and executes the payment.
                 * @method        
                 * @example 
-baasicShoppingCartPaymentService.createByUserIdAndProductId(<user-id>, <product-id>,{
- quantity : '<quantity>',
- timeStamp : '<time-stamp>'
+baasicShoppingCartPaymentService.processCart({
+ customerId : '<customer-id>',
+ systemName : '<system-name>',
+ coupon : '<coupon>'
 })
 .success(function (data) {
  // perform success action here
@@ -44,70 +44,15 @@ baasicShoppingCartPaymentService.createByUserIdAndProductId(<user-id>, <product-
  // perform error handling here
 });
                 **/
-                createByUserIdAndProductId: function (userId, productId, data) {
-                    return baasicApp.shoppingCartModule.items.create(userId, productId, data);
+                processCart: function (data) {
+                    return baasicApp.shoppingCartModule.payment.processCart(data);
                 },
-/**
-                * Returns a promise that is resolved once the remove shopping cart item action has been performed. If the action is successfully completed, the shopping cart itemresource will be permanently removed from the system. This route uses HAL enabled objects to obtain routes and therefore it doesn't apply `baasicShoppingCartItemRouteService` route template. Here is an example of how a route can be obtained from HAL enabled objects:
-```
-var params = baasicApiService.removeParams(shoppingCartItem);
-var uri = params['model'].links('delete').href;
-```
-                * @method        
-                * @example 
-// shopping cart item is a resource previously fetched using get action.				 
-baasicShoppingCartPaymentService.remove(shoppingCartItemId)
-.success(function (data) {
- // perform success action here
-})
-.error(function (response, status, headers, config) {
- // perform error handling here
-});		
-               **/
-                remove: function (id) {
-                    return baasicApp.shoppingCartModule.items.remove(id);
-                },
-/**
-                * Returns a promise that is resolved once the remove shopping cart item action has been performed. If the action is successfully completed, the shopping cart itemresource will be permanently removed from the system. This route uses HAL enabled objects to obtain routes and therefore it doesn't apply `baasicShoppingCartItemRouteService` route template. Here is an example of how a route can be obtained from HAL enabled objects:
-```
-var params = baasicApiService.removeParams(shoppingCartItem);
-var uri = params['model'].links('delete').href;
-```
-                * @method        
-                * @example 
-// shopping cart item is a resource previously fetched using get action.				 
-baasicShoppingCartPaymentService.removeByUserIdAndProductId(<userId>, <productId>)
-.success(function (data) {
- // perform success action here
-})
-.error(function (response, status, headers, config) {
- // perform error handling here
-});		
-               **/
-                removeByUserIdAndProductId: function (userId, productId) {
-                    return baasicApp.shoppingCartModule.items.removeByUserIdAndProductId(userid, productId);
-                },
-/**
-                * Returns a promise that is resolved once the purge items action has been performed. Please note that all shopping cart itemresources will be deleted from the system once the action is successfully completed and therefore it can only be executed by user assigned to account owner role. 
-                * @method        
-                * @example 	 
-baasicShoppingCartPaymentService.purge({})
-.success(function (data) {
- // perform success action here
-})
-.error(function (response, status, headers, config) {
- // perform error handling here
-});		
-               **/
-                purge: function () {
-                    return baasicApp.shoppingCartModule.items.purge();
-                },                
-                /**
+               /**
                  * Provides direct access to `baasicShoppingCartItemRouteService`.
                  * @method        
                  * @example baasicShoppingCartPaymentService.routeService.get(expandObject);
                  **/
-                routeService: baasicApp.shoppingCartModule.items.routeDefinition
+                routeService: baasicApp.shoppingCartModule.payment.routeDefinition
             };
         }
     ]);
